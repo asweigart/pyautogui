04937e5e7dae3c4696324cb0e89ac51acf8329dc
Al Sweigart <asweigart@gmail.com>
2014-08-29 22:03:01 -0700

    Updated the list of compatible Python versions.

--- setup.py
+++ setup.py
@@ -28,8 +28,13 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
 )
