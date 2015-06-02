6f877ae7f4c147aa249d8dd713808bd13c7d3bfd
Al Sweigart <asweigart@gmail.com>
2015-06-01 17:59:15 -0700

    Corrected setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.4'
     ],
 )
