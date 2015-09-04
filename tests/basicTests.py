5a13dafeb956ed05bc7cc308b76936620943b60c
Denilson Sá <denilsonsa@gmail.com>
2015-09-04 09:31:41 -0300

    Fixing sys.path insertion.
    The tests can now be run from whatever directory. No need to "cd" first
    to the tests/ directory.

--- tests/basicTests.py
+++ tests/basicTests.py
@@ -4,7 +4,8 @@ import os
 import time
 import threading
 from PIL import Image
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pyautogui
 
 runningOnPython2 = sys.version_info[0] == 2
