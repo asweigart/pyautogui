0e31129629a85e816a14a00efa28e8a72f5c3ddc
Fornost461 <Fornost461@users.noreply.github.com>
2019-04-08 00:03:30 +0200

    Update __init__.py
    a few fixes in a docstring: spelling, grammar, punctuation, case

--- pyautogui/__init__.py
+++ pyautogui/__init__.py
@@ -998,8 +998,8 @@ def press(keys, presses=1, interval=0.0, pause=None, _pause=True):
     Args:
       key (str, list): The key to be pressed. The valid names are listed in
       KEYBOARD_KEYS. Can also be a list of such strings.
-      presses (integer, optiional): the number of press repetition
-      1 by default, for just one press
+      presses (integer, optional): The number of press repetitions.
+      1 by default, for just one press.
       interval (float, optional): How many seconds between each press.
       0.0 by default, for no pause between presses.
       pause (float, optional): How many seconds in the end of function process.
