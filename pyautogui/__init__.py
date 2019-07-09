93e0948e86c1e17fc8c57799300fdac82f3a6285
Al Sweigart <asweigart@gmail.com>
2019-07-08 21:24:55 -0700

    Changed displayMousePosition() so that it doesn't print backspaces in idle.

--- pyautogui/__init__.py
+++ pyautogui/__init__.py
@@ -306,7 +306,9 @@ def size():
 
 
 def onScreen(x, y=None):
-    """Returns whether the given xy coordinates are on the screen or not.
+    """Returns whether the given xy coordinates are on the primary screen or not.
+
+    Note that this function doesn't work for secondary screens.
 
     Args:
       Either the arguments are two separate values, first arg for x and second
@@ -1225,16 +1227,21 @@ def _failSafeCheck():
 def displayMousePosition(xOffset=0, yOffset=0):
     """This function is meant to be run from the command line. It will
     automatically display the location and RGB of the mouse cursor."""
+    try:
+        runningIDLE = sys.stdin.__module__.startswith('idlelib')
+    except:
+        runningIDLE = False
+
     print('Press Ctrl-C to quit.')
     if xOffset != 0 or yOffset != 0:
         print('xOffset: %s yOffset: %s' % (xOffset, yOffset))
-    resolution = size()
     try:
         while True:
             # Get and print the mouse coordinates.
             x, y = position()
             positionStr = 'X: ' + str(x - xOffset).rjust(4) + ' Y: ' + str(y - yOffset).rjust(4)
-            if (x - xOffset) < 0 or (y - yOffset) < 0 or (x - xOffset) >= resolution[0] or (y - yOffset) >= resolution[1]:
+            if not onScreen(x - xOffset, y - yOffset) or sys.platform == 'darwin':
+                # Pixel color can only be found for the primary monitor, and also not on mac due to the screenshot having the mouse cursor in the way.
                 pixelColor = ('NaN', 'NaN', 'NaN')
             else:
                 pixelColor = pyscreeze.screenshot().getpixel((x, y)) # NOTE: On Windows & Linux, getpixel() returns a 3-integer tuple, but on macOS it returns a 4-integer tuple.
@@ -1242,7 +1249,13 @@ def displayMousePosition(xOffset=0, yOffset=0):
             positionStr += ', ' + str(pixelColor[1]).rjust(3)
             positionStr += ', ' + str(pixelColor[2]).rjust(3) + ')'
             sys.stdout.write(positionStr)
-            sys.stdout.write('\b' * len(positionStr))
+            if not runningIDLE:
+                # If this is a terminal, than we can erase the text by printing \b backspaces.
+                sys.stdout.write('\b' * len(positionStr))
+            else:
+                # If this isn't a terminal (i.e. IDLE) then we can only append more text. Print a newline instead and pause a second (so we don't send too much output).
+                sys.stdout.write('\n')
+                time.sleep(1)
             sys.stdout.flush()
     except KeyboardInterrupt:
         sys.stdout.write('\n')
