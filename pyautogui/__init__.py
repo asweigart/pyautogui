1f27f358b7b44091bef001d1ced433be04dc046a
Jovane Castro <jovanecastro12@gmail.com>
2021-04-27 03:07:37 -0300

    Improvments of write( ) method
    Code documentations and list-support added.

--- pyautogui/__init__.py
+++ pyautogui/__init__.py
@@ -1651,32 +1651,29 @@ def hold(keys, logScreenshot=None, _pause=True):
 
 
 @_genericPyAutoGUIChecks
-def typewrite(message, interval=0.0, logScreenshot=None, _pause=True):
-    """Performs a keyboard key press down, followed by a release, for each of
-    the characters in message.
+def typewrite(message, logScreenshot=None, _pause=True):
+    """Performs a copy and paste of the message.
 
     The message argument can also be list of strings, in which case any valid
     keyboard name can be used.
 
-    Since this performs a sequence of keyboard presses and does not hold down
-    keys, it cannot be used to perform keyboard shortcuts. Use the hotkey()
-    function for that.
-
     Args:
-      message (str, list): If a string, then the characters to be pressed. If a
-        list, then the key names of the keys to press in order. The valid names
+      message (str, list): If a string, then the characters to be written. If a
+        list, then the key names of the keys to copy and paste in order. The valid names
         are listed in KEYBOARD_KEYS.
-      interval (float, optional): The number of seconds in between each press.
-        0.0 by default, for no pause in between presses.
 
     Returns:
       None
     """
-    interval = float(interval)  # TODO - this should be taken out.
 
     _logScreenshot(logScreenshot, "write", message, folder=".")
-    copy(message)
-    hotkey('ctrl','v')
+    
+    for c in message:
+        copy(c)
+        if platform.system=='Darwin':
+            hotkey('command','v') #For MacOS
+        else:
+            hotkey('ctrl','v') #For Windows / Linux
     copy('') #Clears clipboard
 
 
