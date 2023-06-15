d95864d94aaa995d067612afdf53c92e0592cea4
ggmaddr <ggiangta@gmail.com>
2023-06-14 23:23:33 -0700

    fix rgb for current cursor for mac

--- pyautogui/__init__.py
+++ pyautogui/__init__.py
@@ -1752,13 +1752,13 @@ def displayMousePosition(xOffset=0, yOffset=0):
             # Get and print the mouse coordinates.
             x, y = position()
             positionStr = "X: " + str(x - xOffset).rjust(4) + " Y: " + str(y - yOffset).rjust(4)
-            if not onScreen(x - xOffset, y - yOffset) or sys.platform == "darwin":
-                # Pixel color can only be found for the primary monitor, and also not on mac due to the screenshot having the mouse cursor in the way.
+            if not onScreen(x - xOffset, y - yOffset):
+                # Pixel color can only be found for the primary monitor
+                #Screenshot in Mac fixed at pyscreeze
                 pixelColor = ("NaN", "NaN", "NaN")
             else:
                 pixelColor = pyscreeze.screenshot().getpixel(
-                    (x, y)
-                )  # NOTE: On Windows & Linux, getpixel() returns a 3-integer tuple, but on macOS it returns a 4-integer tuple.
+                    (x, y))  # NOTE: On Windows & Linux, getpixel() returns a 3-integer tuple, but on macOS it returns a 4-integer tuple.
             positionStr += " RGB: (" + str(pixelColor[0]).rjust(3)
             positionStr += ", " + str(pixelColor[1]).rjust(3)
             positionStr += ", " + str(pixelColor[2]).rjust(3) + ")"
